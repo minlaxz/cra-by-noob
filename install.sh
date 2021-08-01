69172be49b4d740a60c48a6279d0168ac5e65075
Min Latt <minminlaxz@gmail.com>
2021-08-01 23:54:18 +0630

    Added other options for cra installation

--- install.sh
+++ install.sh
@@ -2,14 +2,44 @@
 set -e
 set -o noglob
 
-LAXZHOME=$HOME/.laxz
-mkdir -p $LAXZHOME
-if [ -f $LAXZHOME/cra.sh ]; then
-    rm $LAXZHOME/cra.sh
-fi
-curl -fsSL https://raw.githubusercontent.com/minlaxz/cra-by-noob/main/cra.sh -o $LAXZHOME/cra.sh
-chmod 755 $LAXZHOME/cra.sh
-cat<<EOF >>$LAXZHOME/.laxzrc
+setup_laxzhome() {
+    LAXZHOME=$HOME/.laxz
+    mkdir -p $LAXZHOME
+}
+
+remove_cra() {
+    if [ -f $LAXZHOME/cra.sh ]; then
+        rm $LAXZHOME/cra.sh
+    fi
+}
+
+download_cra() {
+    curl -fsSL https://raw.githubusercontent.com/minlaxz/cra-by-noob/main/cra.sh -o $LAXZHOME/cra.sh
+    chmod 755 $LAXZHOME/cra.sh
+}
+
+finish_up() {
+    cat <<EOF >>$LAXZHOME/.laxzrc
 alias cra="$HOME/.laxz/cra.sh"
 EOF
-echo "source $HOME/.laxz/.laxzrc" >> $HOME/.zshrc
+    echo "source $HOME/.laxz/.laxzrc" >>$HOME/.zshrc
+    echo "Good to go!"
+}
+
+main() {
+    echo "run with option : $1"
+    case "$1" in
+    "" | "-i")
+        setup_laxzhome
+        remove_cra
+        download_cra
+        finish_up
+        ;;
+    "-r")
+        remove_cra
+        ;;
+    esac
+
+}
+
+main "$@"
