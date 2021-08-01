0de79ce34c2a3be681c96b593e3691f223aacda3
Min Latt <minminlaxz@gmail.com>
2021-08-02 00:43:15 +0630

    Options and debugging installer

--- install.sh
+++ install.sh
@@ -2,6 +2,30 @@
 set -e
 set -o noglob
 
+# curl -fsSL https://getcra.minlaxz.me | sh -s -- -i // to install cra
+
+HEAD='\e[7;36m'
+RESET='\e[m'
+OUTPUT='\e[32m'
+NL='\n'
+ERROR='\e[3;31m'
+WARN='\e[3;33m'
+
+oneLineOutput() {
+    line=$1
+    echo "${OUTPUT}$line${RESET}"
+}
+
+descriptionOutput() {
+    line=$1
+    echo "${WARN}Description : $line ${RESET}"
+}
+
+warningOutput() {
+    line=$1
+    echo "${ERROR}Warning : $line ${RESET}"
+}
+
 setup_laxzhome() {
     LAXZHOME=$HOME/.laxz
     mkdir -p $LAXZHOME
@@ -9,13 +33,16 @@ setup_laxzhome() {
 
 remove_cra() {
     if [ -f $LAXZHOME/cra.sh ]; then
+        warningOutput "CHECK - found cra."
         rm $LAXZHOME/cra.sh
     fi
+    descriptionOutput "DONE - removed cra."
 }
 
 download_cra() {
     curl -fsSL https://raw.githubusercontent.com/minlaxz/cra-by-noob/main/cra.sh -o $LAXZHOME/cra.sh
     chmod 755 $LAXZHOME/cra.sh
+    descriptionOutput "DONE - downloaded cra."
 }
 
 finish_up() {
@@ -23,19 +50,21 @@ finish_up() {
 alias cra="$HOME/.laxz/cra.sh"
 EOF
     echo "source $HOME/.laxz/.laxzrc" >>$HOME/.zshrc
-    echo "Good to go!"
+    oneLineOutput "Good to go!"
 }
 
 main() {
     echo "run with option : $1"
     case "$1" in
     "" | "-i")
+        oneLineOutput "Installing CRA."
         setup_laxzhome
         remove_cra
         download_cra
         finish_up
         ;;
     "-r")
+        oneLineOutput "Removeing CRA."
         remove_cra
         ;;
     esac
