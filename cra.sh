a7aec476a4af53fe2f6a98f48b2348f5bff3e953
Min Latt <minminlaxz@gmail.com>
2021-08-02 16:34:43 +0630

    Added other options for cra itself

--- cra.sh
+++ cra.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -e
 set -o noglob
 
-# curl ... | Name="Project Name" sh -
 HEAD='\e[7;36m'
 RESET='\e[m'
 OUTPUT='\e[32m'
@@ -12,17 +11,17 @@ WARN='\e[3;33m'
 
 oneLineOutput() {
     line=$1
-    echo "${OUTPUT}$line${RESET}"
+    echo -e "${OUTPUT}$line${RESET}"
 }
 
 descriptionOutput() {
     line=$1
-    echo "${WARN}Description : $line ${RESET}"
+    echo -e "${WARN}Description : $line ${RESET}"
 }
 
 warningOutput() {
     line=$1
-    echo "${ERROR}Warning : $line ${RESET}"
+    echo -e "${ERROR}Warning : $line ${RESET}"
 }
 
 hasSudo() {
@@ -45,14 +44,14 @@ make_project() {
     # Already define a project name
     projectName=$1
 
-    if [ -z $projectName ]; then
+    if [[ -z $projectName ]]; then
         read -p "Enter your project name : " projectName
-        if [ -z $projectName ]; then
+        if [[ -z $projectName ]]; then
             warningOutput "You cannot start a project without a name!"
             exit 1
         else
             descriptionOutput "Checking project already exists..."
-            if [ -d $projectName ]; then
+            if [[ -d $projectName ]]; then
                 warningOutput "'$projectName' already exists at $(pwd)/$projectName"
                 exit 1
             else
@@ -62,7 +61,7 @@ make_project() {
     fi
     oneLineOutput "Lazyyy ones, we are gonna create \033[33mreact app\033[39m without node_modules!\nHere > $(pwd)/$projectName"
     read -p "Version default [0.0.0] : " projectVersion
-    if [ -z "$projectVersion" ]; then
+    if [[ -z "$projectVersion" ]]; then
         version="0.0.0"
     else
         version="$projectVersion"
@@ -71,16 +70,16 @@ make_project() {
 
 check_libs() {
     hasUnzip && hasCurl
-    if [ $? -eq 0 ]; then
+    if [[ $? -eq 0 ]]; then
         descriptionOutput "All needed depedencies're already installed, y're good to go."
-        read -p "Press enter to continue [CTRL-C to cancle] ... " _
+        read -p "Press enter to continue [CTRL-C to cancle] ... "
         oneLineOutput "Please weed :c ... "
     else
         hasSudo
-        if [ $? -eq 0 ]; then
+        if [[ $? -eq 0 ]]; then
             warningOutput "this need 'unzip, curl'\nAnd are going to installed."
             # read -n 1 -s -r -p "Press any key to continue ..."
-            read -p "Press enter to continue [CTRL-C to cancle] ... " _
+            read -p "Press enter to continue [CTRL-C to cancle] ... "
             sudo apt-get install unzip curl
         else
             warningOutput "this need 'sudo, unzip, curl'"
@@ -91,16 +90,6 @@ check_libs() {
 
 create_project() {
     mkdir -p $projectName
-}
-
-# Download bootstrapped files
-download_bootstrapped_files() {
-    descriptionOutput "Getting latest tarball list..."
-    tarballVersion=$(curl -s https://api.github.com/repos/minlaxz/cra-by-noob/releases/latest | grep "browser_download_url.*tar.xz"  | cut -d : -f 2,3 | tr -d \" | xargs)
-    descriptionOutput "Downloading bootstrapped tarball..."
-    curl -fsSL $tarballVersion | tar xfJ - -C $projectName/ && cd $projectName
-    # curl -fsSL https://raw.githubusercontent.com/minlaxz/cra-by-noob/main/cra.tar.xz | tar xfJ - -C $projectName/ && cd $projectName
-    oneLineOutput "DONE - tarball is extracted."
     descriptionOutput "Creating package.json ..."
     cat <<EOF >>package.json
 {
@@ -145,11 +134,114 @@ EOF
     oneLineOutput "DONE - created package.json"
 }
 
+# Download bootstrapped files
+download_bootstrapped_files() {
+    descriptionOutput "Getting latest tarball list..."
+    tarballVersion=$(curl -s https://api.github.com/repos/minlaxz/cra-by-noob/releases/latest | grep "browser_download_url.*tar.xz" | cut -d : -f 2,3 | tr -d \" | xargs)
+    descriptionOutput "Downloading bootstrapped tarball..."
+    curl -fsSL $tarballVersion | tar xfJ - -C $projectName/ && cd $projectName
+    # curl -fsSL https://raw.githubusercontent.com/minlaxz/cra-by-noob/main/cra.tar.xz | tar xfJ - -C $projectName/ && cd $projectName
+    oneLineOutput "DONE - tarball is extracted."
+}
+
+download_cra() {
+    descriptionOutput "Downloading latest version of cra..."
+    curl -fsSL https://raw.githubusercontent.com/minlaxz/cra-by-noob/main/cra.sh -o $LAXZHOME/cra.sh
+    chmod 755 $LAXZHOME/cra.sh
+    oneLineOutput "DONE - cra is downloaded."
+    check_update
+}
+
+check_remote_sha() {
+    descriptionOutput "Checking for update from remote..."
+    remote_checksum=$(curl -fsSL https://raw.githubusercontent.com/minlaxz/cra-by-noob/main/cra.sh | sha256sum | awk '{print $1}' | cut -c -12)
+    oneLineOutput "DONE - Remote Checksum : $remote_checksum"
+}
+
+check_local_sha() {
+    # For multile shell files
+    # local_checksum=$(find $LAXZHOME -maxdepth 2 -name "*.sh" -print0 | xargs -0 sha1sum | sort -h | sha256sum | awk '{print $1}')
+    local_checksum=$(cat $LAXZHOME/cra.sh | sha256sum | awk '{print $1}' | cut -c -12)
+    oneLineOutput "DONE - Local  Checksum : $local_checksum"
+}
+
+check_update() {
+    check_remote_sha
+    check_local_sha
+    if [ "$remote_checksum" != "$local_checksum" ]; then
+        isCraOutdated=1
+    else
+        isCraOutdated=0
+        oneLineOutput "Cra is at latest version $local_checksum :v ."
+    fi
+}
+
+# Remove CRA
+remove_cra() {
+    if [[ -f $LAXZHOME/cra.sh ]]; then
+        rm -rf $LAXZHOME/cra.sh
+        if [[ -f $LAXZHOME/.laxzrc ]]; then
+            sed -i '/alias cra/d' $LAXZHOME/.laxzrc # clean alias about cra in laxzrc
+            oneLineOutput "cra is removed from laxzrc"
+        else
+            warningOutput ".laxzrc is not found to remove alias cra."
+        fi
+    else
+        warningOutput "Seems to be cra is installed at other locations.\nPlease remove it manually. $(which cra)"
+    fi
+}
+
 # --- running process --
 
-{
-    make_project "$@"
-    check_libs
-    create_project
-    download_bootstrapped_files
+# {
+#     make_project "$@"
+#     check_libs
+#     create_project
+#     download_bootstrapped_files
+# }
+
+# test "${VAR}" -eq 1 && action || true
+main() {
+    LAXZHOME=$HOME/.laxz
+
+    case $1 in
+    "-h" | "--help")
+        oneLineOutput "-n --new      Create new lite-react project."
+        cat <<EOF >&1
+    Example : 
+        cra
+        cra -n <projectname>
+        cra --name <projectname>
+EOF
+        oneLineOutput "-u --update   Check for cra update."
+        cat <<EOF >&1
+    Example :
+        cra -u
+        cra --update
+EOF
+        ;;
+
+    "-n" | "--new" | "")
+        make_project "$2"
+        check_libs
+        create_project
+        download_bootstrapped_files
+        ;;
+    "-u" | "--update")
+        check_update
+        if [ $isCraOutdated -eq 1 ]; then
+            read -p "New version found, press Enter to continue ... "
+            download_cra
+        fi
+        ;;
+    "-r" | "--remove")
+        remove_cra
+        ;;
+    *)
+        descriptionOutput "unrecognized '$1', -h for help."
+        ;;
+    esac
+
 }
+
+main "$@"
